19ab7822faecf9ad9c31737f499cf512611f11c4
Iiro Jäppinen <iiro@jappinen.fi>
2017-08-30 12:03:11 +0300

    Improvements to Person

--- client/src/components/Person.js
+++ client/src/components/Person.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
-import styles from './Person.pcss';
+import styled from 'styled-components';
+import cx from 'classnames';
 
 const Person = props => {
-  const { person } = props;
+  const { className, person } = props;
+
+  const classes = cx(className, {
+    male: person.gender === 'm',
+    female: person.gender === 'f',
+  });
 
   return (
-    <article className={styles.person}>
-      <p>
+    <article className={classes}>
+      <p className="name">
         <strong>{person.lastName}</strong>, {person.firstName}
+      </p>
+      <p>
+        <span>{person.gender}</span>, <span>{person.age}</span>
       </p>
     </article>
   );
@@ -20,7 +28,29 @@ Person.propTypes = {
     id: PropTypes.string.isRequired,
     firstName: PropTypes.string.isRequired,
     lastName: PropTypes.string.isRequired,
+    gender: PropTypes.string.isRequired,
+    age: PropTypes.number.isRequired,
   }),
 };
 
-export default Person;
+export default styled(Person)`
+  background-color: hsla(0, 0%, 100%, 1);
+  border-radius: 0.5em;
+  border: 3px solid hsla(0, 0%, 80%, 1);
+  margin: 0.5em 0;
+  padding: 1em;
+
+  &.male {
+    background-color: hsla(0, 40%, 95%, 1);
+  }
+
+  &.female {
+    background-color: hsla(210, 40%, 95%, 1);
+  }
+
+  .name {
+    strong {
+      font-weight: bold;
+    }
+  }
+`;
--- client/src/services/person.js
+++ client/src/services/person.js
@@ -1,11 +1,14 @@
 import uuid from 'uuid';
 import faker from 'faker';
+import random from '../utils/random';
 
 const createPerson = () => {
   return {
     id: uuid(),
     lastName: faker.name.lastName(),
     firstName: faker.name.firstName(),
+    age: random.integer(19, 70),
+    gender: random.pick(['m', 'f']),
   };
 };
 
