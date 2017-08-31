53e1218b3f22be8fc17e7a765e60275f500088c1
Iiro Jäppinen <iiro@jappinen.fi>
2017-08-31 14:48:22 +0300

    Updates

--- client/src/components/ResourcePage/index.js
+++ client/src/components/ResourcePage/index.js
@@ -3,23 +3,37 @@ import { pure } from 'recompose';
 
 import Header from '../Header';
 import Loading from '../Loading';
+import Button from '../form/Button';
 
 const ResourcePage = props => {
-  const { loading, resource } = props;
+  const { loading, resource, delResource } = props;
 
-  if (loading)
+  if (loading) {
     return (
       <div>
         <Header />
         <Loading />
       </div>
     );
+  }
+
+  if (!loading && !resource) {
+    return (
+      <div>
+        <Header />
+        <p>Resurssi-identifikaatiolla {props.match.params.id} ei löytynyt resurssia.</p>
+      </div>
+    );
+  }
 
   return (
     <div>
       <Header />
       <article>
-        <p>{resource.firstName}</p>
+        <p>
+          {resource.firstName} {resource.lastName}
+        </p>
+        <Button onClick={() => delResource(resource.id)}>Poista resurssi</Button>
       </article>
     </div>
   );
--- client/src/components/container/ResourcePageContainer.js
+++ client/src/components/container/ResourcePageContainer.js
@@ -1,15 +1,26 @@
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import { withRouter } from 'react-router';
 
+import { delResource } from '../../ducks/resource';
 import ResourcePage from '../ResourcePage';
 
 export default withRouter(
-  connect((state, props) => {
-    const id = props.match.params.id;
+  connect(
+    (state, props) => {
+      const id = props.match.params.id;
 
-    return {
-      loading: state.resource.get('loading'),
-      resource: state.resource.get('resources').find(resource => resource.id === id),
-    };
-  })(ResourcePage)
+      return {
+        loading: state.resource.get('loading'),
+        resource: state.resource.get('resources').find(resource => resource.id === id),
+      };
+    },
+    dispatch =>
+      bindActionCreators(
+        {
+          delResource,
+        },
+        dispatch
+      )
+  )(ResourcePage)
 );
